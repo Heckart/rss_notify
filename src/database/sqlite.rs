Heckart/rss_notify

commit ebc271c76527262a0451cba4b3bfe4260dffd74b

chore: added compatibility for restriction lints The codebase now passes a wide variety of restriction lints. Changes made involve passing the following: - https://rust-lang.github.io/rust-clippy/rust-1.92.0/index.html#deref_by_slicing - https://rust-lang.github.io/rust-clippy/rust-1.92.0/index.html#infinite_loop - https://rust-lang.github.io/rust-clippy/rust-1.92.0/index.html#pattern_type_mismatch - https://rust-lang.github.io/rust-clippy/rust-1.92.0/index.html#print_stdout - https://rust-lang.github.io/rust-clippy/rust-1.92.0/index.html#std_instead_of_core - https://rust-lang.github.io/rust-clippy/rust-1.92.0/index.html#str_to_string - https://rust-lang.github.io/rust-clippy/rust-1.92.0/index.html#undocumented_unsafe_blocks - https://rust-lang.github.io/rust-clippy/rust-1.92.0/index.html#unwrap_in_result - https://rust-lang.github.io/rust-clippy/rust-1.92.0/index.html#unwrap_used - https://rust-lang.github.io/rust-clippy/rust-1.92.0/index.html#verbose_file_reads

src/env_setup/read_env.rs
@@ -1,7 +1,6 @@
 use log::{debug, trace};
 use std::env::var;
-use std::fs::File;
-use std::io::Read;
+use std::fs::read_to_string;
 
 /// **Purpose**:    Grabs the list of rss feeds in the file represented by an env var
 /// **Parameters**: A &str representing the name of an environment variable holding a feed list file
@@ -12,20 +11,9 @@ use std::io::Read;
 /// **Status**:     Done
 pub fn get_feed_list(feed_env_var: &str) -> Vec<String> {
     trace!("Inside get_feed_list with feed_env_var as {feed_env_var}.");
-    // open the feed url file, specified by the env variable
-    let mut feed_url_file: File = match File::open(source_env_var(feed_env_var)) {
-        Ok(file_name) => {
-            trace!("Opened feed url file.");
-            file_name
-        }
-        Err(err) => {
-            panic!("Could not open feed url file! {err}.")
-        }
-    };
-
     // grab the txt contents of the feed url file
     let mut url_file_contents: String = String::new();
-    match feed_url_file.read_to_string(&mut url_file_contents) {
+    match read_to_string(&mut url_file_contents) {
         Ok(_) => {
             trace!("Read feed url file contents.");
         }

src/fetch/http.rs
@@ -223,9 +223,9 @@ fn get_existing_feed(
     let mut get_client: RequestBuilder = Client::new().get(feed_url);
 
     // maybe change this to do both etag and last_modified if both exist?
-    if let Some(etag) = &existing_db_entry.etag {
+    if let Some(etag) = existing_db_entry.etag {
         get_client = get_client.header(IF_NONE_MATCH, etag);
-    } else if let Some(last_modified) = &existing_db_entry.last_modified {
+    } else if let Some(last_modified) = existing_db_entry.last_modified {
         get_client = get_client.header(IF_MODIFIED_SINCE, last_modified);
     } else {
         // there aren't any saved headers, so we need to pull the full bytes not considering headers

src/main.rs
@@ -2,15 +2,32 @@
 #![warn(clippy::all)]
 #![warn(clippy::pedantic)]
 #![warn(clippy::nursery)]
+#![warn(clippy::restriction)]
 #![deny(warnings)]
+#![allow(clippy::allow_attributes_without_reason)]
+#![allow(clippy::arbitrary_source_item_ordering)]
+#![allow(clippy::blanket_clippy_restriction_lints)]
 #![allow(clippy::doc_markdown)]
+#![allow(clippy::exhaustive_structs)]
+#![allow(clippy::implicit_return)]
 #![allow(clippy::match_bool)]
+#![allow(clippy::missing_docs_in_private_items)]
 #![allow(clippy::missing_errors_doc)]
+#![allow(clippy::missing_inline_in_public_items)]
 #![allow(clippy::missing_panics_doc)]
+#![allow(clippy::mod_module_files)]
+#![allow(clippy::module_name_repetitions)]
 #![allow(clippy::must_use_candidate)]
+#![allow(clippy::panic)]
+#![allow(clippy::pub_use)]
+#![allow(clippy::redundant_type_annotations)]
+#![allow(clippy::single_call_fn)]
 #![allow(clippy::single_match_else)]
 #![allow(clippy::too_long_first_doc_paragraph)]
 use bytes::Bytes;
+use core::error;
+use core::error::Error;
+use core::time::Duration;
 use log::{debug, error, info, trace};
 use reqwest::StatusCode;
 use reqwest::blocking::Response;
@@ -21,10 +38,7 @@ use rss_notify::fetch::fetch_feed_as_bytes;
 use rss_notify::parse::get_new_rss_items;
 use rss_notify::push::{send_failure_notification, send_new_item_notification};
 use rusqlite::Connection;
-use std::error;
-use std::error::Error;
 use std::thread::sleep;
-use std::time::Duration;
 
 /*
 1. Download feed
@@ -41,7 +55,8 @@ use std::time::Duration;
 /// **Tests**:      Not implemented yet
 /// **Status**:     Add support for tracking website changes in addition to rss feed changes,
 ///                 maybe transition program from blocking to async
-fn main() {
+fn main() -> ! {
+#![allow(clippy::cognitive_complexity)] // temporary
     env_logger::init();
     trace!("Starting up!");
 
@@ -63,7 +78,7 @@ fn main() {
                 Ok(bytes) => {
                     if bytes.is_some() {
                         debug!("Sourced feed bytes for {url}.");
-                        // invariant
+                        // SAFETY: we have an earlier check that ensures bytes is Some
                         unsafe { bytes.unwrap_unchecked() }
                     } else {
                         debug!("Feed {url} did not have indicated web page changes. Skipping!");
@@ -112,7 +127,9 @@ fn main() {
                     match response {
                         Ok(ok) => {
                             let status: StatusCode = ok.status();
-                            let body: String = ok.text().unwrap();
+                            let body: String = ok
+                                .text()
+                                .unwrap_or_else(|_| "N/A (Ntfy did not return body)".to_owned());
 
                             if status == StatusCode::OK {
                                 debug!("Ntfy responsed with\nStatus: {status}\nBody:\n{body}\n.");
@@ -195,7 +212,8 @@ fn try_send_failure_notification(errors: &mut Vec<String>, new_error: Option<Str
             debug!(
                 "Ntfy responsed with\nStatus: {}\nBody:\n{}\n.",
                 ok.status(),
-                ok.text().unwrap()
+                ok.text()
+                    .unwrap_or_else(|_| { "N/A (Ntfy did not return body)".to_owned() })
             );
             info!("Able to send error notification, so clearing error vector.");
             errors.clear();

src/parse/rss_feed.rs
@@ -1,6 +1,6 @@
 use crate::database::{DBEntry, get_feed_from_db, insert_feed_to_db};
 use bytes::Bytes;
-use log::{error, trace, warn};
+use log::{debug, error, trace, warn};
 use rss::{Channel, Item, ItemBuilder};
 use rusqlite::Connection;
 use std::error;
@@ -19,6 +19,7 @@ pub fn get_new_rss_items(
     feed_url: &String,
     feed_bytes: &Bytes,
 ) -> Result<Vec<Item>, Box<dyn error::Error>> {
+    #![allow(clippy::expect_used)] // temporary
     trace!("Inside get_new_rss_items.");
 
     let db_feed_items: Vec<Item> = match get_feed_from_db(conn, feed_url) {
@@ -43,7 +44,7 @@ pub fn get_new_rss_items(
         }
     };
 
-    let new_feed_channel: Channel = match Channel::read_from(&feed_bytes[..]) {
+    let new_feed_channel: Channel = match Channel::read_from(&**feed_bytes) {
         Ok(channel) => {
             trace!("Successfully converted {feed_url} feed bytes into rss channel.");
             normalize_rss_items_in_channel(channel)
@@ -103,7 +104,7 @@ pub fn get_new_rss_items(
 /// **Status**:     Done
 pub fn stringify_feed_bytes(feed_bytes: &Bytes) -> Result<String, Box<dyn error::Error>> {
     trace!("Inside serialize_feed_bytes");
-    let rss_channel: Channel = match Channel::read_from(&feed_bytes[..]) {
+    let rss_channel: Channel = match Channel::read_from(&**feed_bytes) {
         Ok(result) => {
             trace!("Successfully converted feed bytes to rss_channel.");
             normalize_rss_items_in_channel(result)
@@ -116,7 +117,7 @@ pub fn stringify_feed_bytes(feed_bytes: &Bytes) -> Result<String, Box<dyn error:
 
     let serialized: String = match serde_json::to_string(&rss_channel.items().to_vec()) {
         Ok(json) => {
-            trace!("Successfully stringified feed bytes.");
+            debug!("Successfully stringified feed bytes as {json}.");
             json
         }
         Err(err) => {
@@ -125,7 +126,6 @@ pub fn stringify_feed_bytes(feed_bytes: &Bytes) -> Result<String, Box<dyn error:
         }
     };
 
-    println!("new method serialized: {serialized}");
     Ok(serialized)
 }
 
@@ -168,8 +168,8 @@ fn normalize_rss_items_in_channel(channel: Channel) -> Channel {
 
         new_items.push(
             ItemBuilder::default()
-                .title(Some(item_title.to_string()))
-                .link(Some(item_link.to_string()))
+                .title(Some(item_title.to_owned()))
+                .link(Some(item_link.to_owned()))
                 .build(),
         );
     }
@@ -211,6 +211,7 @@ fn make_new_item_vector(old_items: &[Item], new_channel: &Channel) -> Vec<Item> 
 /// **Tests**:      Not implemented yet
 /// **Status**:     Done
 pub fn print_serialized_rss(items: &[Item]) {
+    #![allow(clippy::print_stdout)]
     trace!("Inside print_serialized_rss with  {} items.", items.len());
     if !items.is_empty() {
         for item in items {

src/lib.rs
@@ -1,8 +1,10 @@
 #![warn(clippy::all)]
 #![warn(clippy::pedantic)]
 #![warn(clippy::nursery)]
+#![warn(clippy::restriction)]
 #![deny(warnings)]
 #![allow(clippy::allow_attributes_without_reason)]
+#![allow(clippy::arbitrary_source_item_ordering)]
 #![allow(clippy::blanket_clippy_restriction_lints)]
 #![allow(clippy::doc_markdown)]
 #![allow(clippy::exhaustive_structs)]
@@ -13,13 +15,13 @@
 #![allow(clippy::missing_inline_in_public_items)]
 #![allow(clippy::missing_panics_doc)]
 #![allow(clippy::mod_module_files)]
+#![allow(clippy::module_name_repetitions)]
 #![allow(clippy::must_use_candidate)]
 #![allow(clippy::panic)]
 #![allow(clippy::pub_use)]
 #![allow(clippy::redundant_type_annotations)]
 #![allow(clippy::single_call_fn)]
 #![allow(clippy::single_match_else)]
-#![allow(clippy::std_instead_of_core)]
 #![allow(clippy::too_long_first_doc_paragraph)]
 pub mod database;
 pub mod env_setup;

src/push/ntfy.rs
@@ -28,11 +28,11 @@ pub fn send_new_item_notification(
         let article_title: String = item
             .title
             .as_ref()
-            .map_or_else(|| "(NO TITLE)".to_string(), Clone::clone);
+            .map_or_else(|| "(NO TITLE)".to_owned(), Clone::clone);
         let article_url: String = item
             .link
             .as_ref()
-            .map_or_else(|| "(NO URL)".to_string(), Clone::clone);
+            .map_or_else(|| "(NO URL)".to_owned(), Clone::clone);
 
         let push_title: String = format!("NEW ARTICLE: {article_title}");
 
@@ -89,5 +89,6 @@ pub fn send_failure_notification(
         return Err(Box::new(result_err));
     }
 
-    Ok(result.unwrap())
+    // SAFETY: The above line ensures this is not Err
+    unsafe { Ok(result.unwrap_unchecked()) }
 }

commit 753f887ed923f770e45f5d46338164ff3cecb9e1

feat: offload header updates from the fetch library to the parse library Previously, when an existing feed had header changes, the fetch library would update the DB entry for the headers and then the parse library would update the history if there were changes. This was not ideal because if parsing failed, the history wouldn't be updated, but the headers would have already been updated. This change ensured the state of a DB row at any given time is fully synced between all the columns. This change also removed the need for the DBEntry struct to have the history as an Option<String>, which in turn removed the need for a .expect(). The DB library upsert function no longer COALESCEs anything.
This commit also includes a fix for a regression introduced by commit
ebc271c which caused failure to source the feed history env var.

src/database/sqlite.rs
@@ -4,7 +4,7 @@ use rusqlite::{Connection, params};
 
 pub struct DBEntry {
     pub feed_name: String,
-    pub history: Option<String>, // history has a not NULL constraint so there is always a history in the feed, but we may be passing a None in circumstances dictated by http.rs
+    pub history: String,
     pub last_modified: Option<String>,
     pub etag: Option<String>,
 }
@@ -182,10 +182,7 @@ pub fn get_feed_from_db(
 /// **Panics**:     No
 /// **Modifies**:   Creates or updates a row in the db with the new_row's feed_name column
 /// **Tests**:      Not implemented yet
-/// **Status**:     Done? I've gone back and forth several times on whether or not to have one
-///                 function that handles all of this as is currently done, or to have three separate
-///                 functions: one for cerating new rows, one for updating headers only (post fetch),
-///                 and one for updating history only (post parse).
+/// **Status**:     Done?
 pub fn insert_feed_to_db(conn: &Connection, new_row: &DBEntry) -> Result<usize, rusqlite::Error> {
     trace!(
         "Inside insert_feed_to_db inserting feed {}.",
@@ -194,20 +191,11 @@ pub fn insert_feed_to_db(conn: &Connection, new_row: &DBEntry) -> Result<usize, 
 
     match conn.execute(
         "INSERT INTO feed_hist (feed_name, history, last_modified, etag)
-        VALUES (
-            ?1,
-            COALESCE(?2,
-                (SELECT history FROM feed_hist WHERE feed_name = ?1)
-            ),
-            ?3,
-            ?4
-        )
+        VALUES (?1, ?2, ?3, ?4)
         ON CONFLICT(feed_name) DO UPDATE SET
-            history = COALESCE(?2, feed_hist.history),
-            last_modified = COALESCE(?3, feed_hist.last_modified),
-            etag = COALESCE(?4, feed_hist.etag)
-            ", // COALESCEing the headers probably isn't good. This stops a header from going null if a feed used to have one and since discarded it.
-        //TODO: change it!
+            history = ?2,
+            last_modified = ?3,
+            etag = ?4",
         params!(
             new_row.feed_name,
             new_row.history,

src/env_setup/read_env.rs
@@ -11,16 +11,17 @@ use std::fs::read_to_string;
 /// **Status**:     Done
 pub fn get_feed_list(feed_env_var: &str) -> Vec<String> {
     trace!("Inside get_feed_list with feed_env_var as {feed_env_var}.");
+
     // grab the txt contents of the feed url file
-    let mut url_file_contents: String = String::new();
-    match read_to_string(&mut url_file_contents) {
-        Ok(_) => {
+    let url_file_contents: String = match read_to_string(source_env_var(feed_env_var)) {
+        Ok(contents) => {
             trace!("Read feed url file contents.");
+            contents
         }
         Err(err) => {
             panic!("Could not convert file contents to string! {err}.");
         }
-    }
+    };
 
     // construct a vector of all the urls
     let url_list: Vec<String> = url_file_contents

src/fetch/http.rs
@@ -16,19 +16,27 @@ struct ResponseDetails {
     pub first_time_feed: bool,
 }
 
+/// Information to be passed from the fetch library to the parse library. Contains the feed bytes as
+/// well as the relevant headers if they exist
+pub struct FeedBytesAndHeaders {
+    pub bytes: Bytes,
+    pub etag: Option<String>,
+    pub last_modified: Option<String>,
+}
+
 /// **Purpose**:    Grab the bytes of rss feed content if new content is available
 /// **Parameters**: A &rusqlite::Connection for the db connection, a &String containing an rss feed URL
-/// **Ok Return**:  An Option<Bytes> object of rss content if a new GET request was made
+/// **Ok Return**:  An Option<FeedBytesAndHeaders> object of rss content and headers if a new GET
+///                 request was made and new content is expected
 /// **Err Return**: A Box<dyn error::Error> from a GET request or DB query
 /// **Panics**:     No
-/// **Modifies**:   Creates a new DB row if the feed didn't already have an entry, updates the DB
-///                 row headers if there are changes in the feed
+/// **Modifies**:   Creates a new DB row if the feed didn't already have an entry
 /// **Tests**:      Not implemented yet
 /// **Status**:     Done
 pub fn fetch_feed_as_bytes(
     conn: &Connection,
     feed_url: &String,
-) -> Result<Option<Bytes>, Box<dyn error::Error>> {
+) -> Result<Option<FeedBytesAndHeaders>, Box<dyn error::Error>> {
     trace!("Inside fetch_feed_as_bytes with feed_url of {feed_url}.");
 
     let feed_response: ResponseDetails;
@@ -66,35 +74,35 @@ pub fn fetch_feed_as_bytes(
         Err(err) => return Err(Box::new(err)),
     }
 
-    let mut returned_feed_bytes: Option<Bytes> = None;
+    let returned_feed_elements: FeedBytesAndHeaders;
 
-    if let Some(new_feed_bytes) = feed_response.bytes.clone() {
-        returned_feed_bytes = Some(new_feed_bytes);
-
-        // The only time this function should update the feed history is if the feed doesn't already
-        // exist in the data base
-        let bytes_to_stringify: Option<Bytes> = match feed_response.first_time_feed {
-            false => None,
-            true => feed_response.bytes,
+    // The bytes condition is what determines whether or not there is genuinely new content
+    // If not, we will just return a None value
+    if let Some(new_feed_bytes) = feed_response.bytes {
+        let returned_feed_bytes: Bytes = new_feed_bytes;
+        returned_feed_elements = FeedBytesAndHeaders {
+            bytes: returned_feed_bytes,
+            etag: feed_response.etag,
+            last_modified: feed_response.last_modified,
         };
-        match update_db_with_new_feed_info(
-            conn,
-            feed_url,
-            bytes_to_stringify,
-            feed_response.etag,
-            feed_response.last_modified,
-        ) {
-            Ok(_) => {
-                trace!("Successful update to DB for {feed_url}.");
-            }
-            Err(err) => {
-                error!("Could not update DB for {feed_url}.");
-                return Err(err);
+
+        // only calling this function if we know its the first time the feed has been seen
+        if feed_response.first_time_feed {
+            match update_db_with_new_feed_info(conn, feed_url, &returned_feed_elements) {
+                Ok(_) => {
+                    trace!("Successful update to DB for {feed_url}.");
+                    return Ok(None); // first time feed means return nothing back to main
+                }
+                Err(err) => {
+                    error!("Could not update DB for {feed_url}.");
+                    return Err(err);
+                }
             }
         }
+        return Ok(Some(returned_feed_elements));
     }
-
-    Ok(returned_feed_bytes)
+    // no bytes were returned from earlier, so we know there is no new content to parse
+    Ok(None)
 }
 
 /// **Purpose**:    Helper function to perform a GET request on a rss feed url and return useful
@@ -116,7 +124,7 @@ fn make_get_request(
 
     let request_result: Result<Response, reqwest::Error>;
 
-// tracking purposes in fetch_feed_as_bytes(), makes that function simpler 
+    // tracking purposes in fetch_feed_as_bytes(), makes that function simpler
     let first_time_seeing_feed: bool = if let Some(get_request) = get_client {
         request_result = RequestBuilder::send(get_request);
         false
@@ -144,7 +152,7 @@ fn make_get_request(
             debug!(
                 "GET request for {feed_url} had matching headers! Assuming no change to feed contents."
             );
-            //TODO: Can probably still update headers?
+            //TODO: Can probably still update headers? But this complicates things on the upsert...
             response_etag = None;
             response_last_modified = None;
             response_bytes = None;
@@ -236,43 +244,36 @@ fn get_existing_feed(
 }
 
 /// **Purpose**:    Update or create DB entry for a feeds headers + contents if feed is new
-/// **Parameters**: A &rusqlite::Connection for the db connection, a &String with feed name, a
-///                 Bytes object with new feed bytes, two Option<String>s for the etag and
-///                 last_modified headers
+/// **Parameters**: A &rusqlite::Connection for the db connection, a &FeedBytesAndHeaders with the
+///                 bytes amd headers from recent feed pull
 /// **Ok Return**:  A usize with row change count from the DB
 /// **Err Return**: A Box<dyn error::Error> from failure stringify bytes or to read the DB
 /// **Panics**:     No
-/// **Modifies**:   Creates or updates a row in the DB
+/// **Modifies**:   Creates a row in the DB
 /// **Tests**:      Not implemented yet
 /// **Status**:     Done
 fn update_db_with_new_feed_info(
     conn: &Connection,
     feed_url: &String,
-    feed_bytes: Option<Bytes>,
-    feed_etag: Option<String>,
-    feed_last_modified: Option<String>,
+    feed_bytes_headers: &FeedBytesAndHeaders,
 ) -> Result<usize, Box<dyn error::Error>> {
     trace!("Inside update_db_with_new_feed_info().");
 
-    let mut new_row: DBEntry = DBEntry {
+    let new_row: DBEntry = DBEntry {
         feed_name: feed_url.clone(),
-        history: None,
-        last_modified: feed_last_modified,
-        etag: feed_etag,
-    };
-
-    if let Some(new_bytes) = feed_bytes {
-        new_row.history = match stringify_feed_bytes(&new_bytes) {
+        history: match stringify_feed_bytes(&feed_bytes_headers.bytes) {
             Ok(feed_history) => {
                 trace!("Received rss feed history string.");
-                Some(feed_history)
+                feed_history
             }
             Err(err) => {
                 error!("Could not stringify feed_bytes from ().");
                 return Err(err);
             }
-        }
-    }
+        },
+        last_modified: feed_bytes_headers.last_modified.clone(),
+        etag: feed_bytes_headers.etag.clone(),
+    };
 
     match insert_feed_to_db(conn, &new_row) {
         Ok(rc) => {

src/fetch/mod.rs
@@ -1,4 +1,5 @@
 mod http;
 pub use bytes;
 pub use http::fetch_feed_as_bytes;
+pub use http::FeedBytesAndHeaders;
 pub use reqwest;

src/main.rs
@@ -3,7 +3,7 @@
 #![warn(clippy::pedantic)]
 #![warn(clippy::nursery)]
 #![warn(clippy::restriction)]
-#![deny(warnings)]
+//#![deny(warnings)]
 #![allow(clippy::allow_attributes_without_reason)]
 #![allow(clippy::arbitrary_source_item_ordering)]
 #![allow(clippy::blanket_clippy_restriction_lints)]
@@ -24,17 +24,16 @@
 #![allow(clippy::single_call_fn)]
 #![allow(clippy::single_match_else)]
 #![allow(clippy::too_long_first_doc_paragraph)]
-use bytes::Bytes;
-use core::error;
-use core::error::Error;
-use core::time::Duration;
+use std::error;
+use std::error::Error;
+use std::time::Duration;
 use log::{debug, error, info, trace};
 use reqwest::StatusCode;
 use reqwest::blocking::Response;
 use rss::Item;
 use rss_notify::database::setup_db;
 use rss_notify::env_setup::get_feed_list;
-use rss_notify::fetch::fetch_feed_as_bytes;
+use rss_notify::fetch::{FeedBytesAndHeaders, fetch_feed_as_bytes};
 use rss_notify::parse::get_new_rss_items;
 use rss_notify::push::{send_failure_notification, send_new_item_notification};
 use rusqlite::Connection;
@@ -56,7 +55,7 @@ use std::thread::sleep;
 /// **Status**:     Add support for tracking website changes in addition to rss feed changes,
 ///                 maybe transition program from blocking to async
 fn main() -> ! {
-#![allow(clippy::cognitive_complexity)] // temporary
+    #![allow(clippy::cognitive_complexity)] // temporary
     env_logger::init();
     trace!("Starting up!");
 
@@ -74,12 +73,12 @@ fn main() -> ! {
         trace!("At the top of the main loop.");
         for url in &feed_urls {
             // get the feed contents from the url
-            let feed_bytes: Bytes = match fetch_feed_as_bytes(&conn, &url.clone()) {
-                Ok(bytes) => {
-                    if bytes.is_some() {
+            let feed_elements: FeedBytesAndHeaders = match fetch_feed_as_bytes(&conn, url) {
+                Ok(elements) => {
+                    if elements.is_some() {
                         debug!("Sourced feed bytes for {url}.");
                         // SAFETY: we have an earlier check that ensures bytes is Some
-                        unsafe { bytes.unwrap_unchecked() }
+                        unsafe { elements.unwrap_unchecked() }
                     } else {
                         debug!("Feed {url} did not have indicated web page changes. Skipping!");
                         continue;
@@ -95,7 +94,7 @@ fn main() -> ! {
 
             // find any new items from the feed
             debug!("Looking for new items in {url}.");
-            let feed_items: Vec<Item> = match get_new_rss_items(&conn, &url.clone(), &feed_bytes) {
+            let feed_items: Vec<Item> = match get_new_rss_items(&conn, url, &feed_elements) {
                 Ok(items) => {
                     debug!("Grabbed feed items from {url}.");
                     items
@@ -115,9 +114,8 @@ fn main() -> ! {
                 info!("No new items found for {url} since last check.");
             } else {
                 info!(
-                    "{} new feed items exist from {}, so sending pushes.",
-                    feed_items.len(),
-                    url
+                    "{} new feed items exist from {url}, so sending pushes.",
+                    feed_items.len()
                 );
 
                 let push_results: Vec<Result<Response, Box<dyn error::Error>>> =
@@ -143,7 +141,7 @@ fn main() -> ! {
                             error!(
                                 "send_new_item_notification: Initial response had errors: {err_msg}."
                             );
-                            errors.push(err_msg.clone());
+                            errors.push(err_msg);
                             debug!("Total errors are {}.", errors.len());
                         }
                     }

src/parse/rss_feed.rs
@@ -1,4 +1,5 @@
 use crate::database::{DBEntry, get_feed_from_db, insert_feed_to_db};
+use crate::fetch::FeedBytesAndHeaders;
 use bytes::Bytes;
 use log::{debug, error, trace, warn};
 use rss::{Channel, Item, ItemBuilder};
@@ -6,7 +7,8 @@ use rusqlite::Connection;
 use std::error;
 
 /// **Purpose**:    Grab new items from an rss feed and maintains DB feed history
-/// **Parameters**: A &rusqlite:Connection of the history db, a &String of the feed url, A Bytes object of rss content
+/// **Parameters**: A &rusqlite:Connection of the history db, a &String of the feed url, A
+///                 &FeedBytesAndHeaders object with bytes contents and optional headers
 /// **Ok Return**:  A Vec<rss::Item> of previously unseen rss content
 /// **Err Return**: A Box<dyn error::Error> from failure to serialize feed, failure to get feed
 ///                 bytes, or failure to access the DB
@@ -17,18 +19,13 @@ use std::error;
 pub fn get_new_rss_items(
     conn: &Connection,
     feed_url: &String,
-    feed_bytes: &Bytes,
+    feed_elements: &FeedBytesAndHeaders,
 ) -> Result<Vec<Item>, Box<dyn error::Error>> {
     #![allow(clippy::expect_used)] // temporary
     trace!("Inside get_new_rss_items.");
 
     let db_feed_items: Vec<Item> = match get_feed_from_db(conn, feed_url) {
-        Ok(response) => match serde_json::from_str(
-            response
-                .history
-                .expect("DB invariant violation: history must be NOT NULL.")
-                .as_str(),
-        ) {
+        Ok(response) => match serde_json::from_str(response.history.as_str()) {
             Ok(items) => {
                 trace!("Successfully serialized {feed_url} rss items from DB.");
                 items
@@ -44,7 +41,7 @@ pub fn get_new_rss_items(
         }
     };
 
-    let new_feed_channel: Channel = match Channel::read_from(&**feed_bytes) {
+    let new_feed_channel: Channel = match Channel::read_from(&*feed_elements.bytes) {
         Ok(channel) => {
             trace!("Successfully converted {feed_url} feed bytes into rss channel.");
             normalize_rss_items_in_channel(channel)
@@ -61,18 +58,18 @@ pub fn get_new_rss_items(
     if !new_items.is_empty() {
         let updated_row: DBEntry = DBEntry {
             feed_name: feed_url.clone(),
-            history: match stringify_feed_bytes(feed_bytes) {
+            history: match stringify_feed_bytes(&feed_elements.bytes) {
                 Ok(feed_hist) => {
                     trace!("Successfully stringified feed bytes for insertion to DB");
-                    Some(feed_hist)
+                    feed_hist
                 }
                 Err(err) => {
                     error!("Could not stringify feed bytes for insertion to DB");
                     return Err(err);
                 }
             },
-            last_modified: None,
-            etag: None,
+            last_modified: feed_elements.last_modified.clone(),
+            etag: feed_elements.etag.clone(),
         };
 
         // technically we don't have to return the error here, but it makes more sense to.
@@ -94,7 +91,7 @@ pub fn get_new_rss_items(
 
 /// **Purpose**:    Serialize a Bytes object of rss content into a String with only title and link
 ///                 taken from each rss item
-/// **Parameters**: A Bytes object of rss content
+/// **Parameters**: A &Bytes object of rss content
 /// **Ok Return**:  A String of rss content
 /// **Err Return**: A Box<dyn error::Error) if bytes cannot be converted to rss channel or rss::Item
 ///                 vector can't be serialized
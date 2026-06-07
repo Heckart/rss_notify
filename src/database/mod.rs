76828e683ea62b04d465ce419453830fb6a13906
Ethan Heckart <heckart@proton.me>
2026-06-07 17:48:42 -0500

    style: fix rust formatting Before implementing CI formatting check, ensure rust code is properly formatted according to cargo fmt

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -4,5 +4,5 @@ pub use sqlite::DBHeaders;
 pub use sqlite::feed_is_in_db;
 pub use sqlite::get_feed_from_db;
 pub use sqlite::insert_feed_to_db;
-pub use sqlite::update_feed_headers;
 pub use sqlite::setup_db;
+pub use sqlite::update_feed_headers;
